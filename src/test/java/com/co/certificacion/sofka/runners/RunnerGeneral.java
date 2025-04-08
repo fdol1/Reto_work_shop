a0f3e2e4be6805d8b381866c96d9f883591aa9dd
fdol1 <l1_fdo@hotmail.com>
2025-04-08 11:53:12 -0500

    Modificacion del runner para ejecutar por comando

---
+++ src/test/java/com/co/certificacion/sofka/runners/RunnerGeneral.java
@@ -0,0 +1,15 @@
+package com.co.certificacion.sofka.runners;
+
+import io.cucumber.junit.CucumberOptions;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.runner.RunWith;
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = {"com.co.certificacion.sofka.stepdefinitions"},
+        snippets = CucumberOptions.SnippetType.CAMELCASE
+)
+
+public class RunnerGeneral {
+}
